29a0bbfd7cc0741547ea3f141ec41da286855936
zee-creato <zbigniew.kierpiec@proav.com>
2024-10-14 15:52:32 +0100

    Added Arrow to top

--- src/app/Layout/layout/layout.component.ts
+++ src/app/Layout/layout/layout.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "../../Components/header/header.component";
 import { FooterComponent } from "./footer/footer.component";
+import { ToTopComponent } from "../to-top/to-top.component";
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, ToTopComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
